6668488e1ea7fa0836e199af344fa366858514a5
tamorim20 <tamirisamorim@icloud.com>
2022-07-20 15:13:02 -0300

    PO, Massa de dados em lista e relatórios

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,16 +1,36 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
         cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados =>{
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
-        
+
     });
-    
+
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-       
-    
+        EnderecoPage.editarEnderecoFaturamento('Tamiris', 'Ferreira', 'Google', 'Brasil', 'Rua dois', '03', 'São Paulo', 'São Paulo', '08555471', '77777888', 'email@dominio.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it.only('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereco,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
+
